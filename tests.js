cfb4d76361e1c749ecfa02ac05a496e8b905cc55
Reza Lotfi <rezaiscool7@aol.com>
2018-07-27 20:14:37 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,15 +1,15 @@
-var nig = require("./index.js")
+var index = require("./index.js")
 
-if (nig.name === "Susan") {
+if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
-    console.log("Expected: Susan, Received: "+nig.name)
+    console.log("Expected: Susan, Received: "+index.name)
 }
 
 
 
-if (nig.height === 70) {
+if (index.height === 70) {
     console.log("The height is correct")
 } else {
-    console.log("Expected: 70, Received: "+nig.height)
+    console.log("Expected: 70, Received: "+index.height)
 }
